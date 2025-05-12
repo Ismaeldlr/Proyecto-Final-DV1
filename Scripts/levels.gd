bddeff6984156a1bdde706f5beda002a85f3cfd2
Ismael de la Rosa del Castillo <157316333+Ismaeldlr@users.noreply.github.com>
2025-05-11 22:12:14 -0700

    Fix levels menu

--- Scripts/levels.gd
+++ Scripts/levels.gd
@@ -36,3 +36,9 @@ func _on_6_pressed() -> void:
 	await LevelTransition.fade_to_black()
 	get_tree().change_scene_to_file("res://scenes/levels/level_6.tscn")
 	LevelTransition.fade_from_black()
+
+
+func _on_button_pressed() -> void:
+	await LevelTransition.fade_to_black()
+	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
+	LevelTransition.fade_from_black()
